71ca7804590eea99d15996cf048ad8bd7c22d73b
Stefan Dunikovskyi <stefan090304@gmail.com>
2022-04-07 10:59:05 +0200

    fix/modal

--- src/modal.js
+++ src/modal.js
@@ -1,11 +1,13 @@
 (() => {
   const refs = {
-      openModalBtn: document.querySelector("[data-modal-open]"),
+    openModalBtn: document.querySelector("[data-modal-open]"), 
+  openModalBtnMob: document.querySelector("[data-modal-open-mob]"),
     closeModalBtn: document.querySelector("[data-modal-close]"),
     modal: document.querySelector("[data-modal]"),
   };
 
   refs.openModalBtn.addEventListener("click", toggleModal);
+  refs.openModalBtnMob.addEventListener("click", toggleModal);
   refs.closeModalBtn.addEventListener("click", toggleModal);
 
   function toggleModal() {
